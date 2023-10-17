7509d2bf81bcc94293824c8e37bb635f71ded65d
Yuki <sdt22csc0018@edu.sca.ac.jp>
2023-10-17 12:29:08 +0900

    SelectButton

--- Assets/SelectButton.cs
+++ Assets/SelectButton.cs
@@ -6,12 +6,23 @@ using UnityEngine.EventSystems;
 
 public class SelectButton : MonoBehaviour
 {
+
+    [SerializeField] private Button VegetableButton;
+    [SerializeField] private Button FishMeatButton;
+    [SerializeField] private Button OthersButton;
+    [SerializeField] private GameObject VegetableShelf;
+    [SerializeField] private GameObject FishMeatShelf;
+    [SerializeField] private GameObject OthersShelf;
+
     private Button button;
+
     int TagNum;
     // Start is called before the first frame update
     void Start()
     {
-        button=GetComponent<Button>();
+        //VegetableButton = GetComponent<Button>();
+        //FishMeatButton = GetComponent<Button>();
+        //OthersButton = GetComponent<Button>();
 
         string Tag = this.gameObject.tag;
         if (Tag == "VegetableButton")
@@ -30,21 +41,41 @@ public class SelectButton : MonoBehaviour
               TagNum = 3;
             Debug.Log(Tag);
         }
+
+        button= GetComponent<Button>();
+        button.onClick.AddListener(Select);
+
+        //VegetableButton.onClick.AddListener(Select);
+        //FishMeatButton.onClick.AddListener(Select);
+        //OthersButton.onClick.AddListener(Select);
     }
 
     // Update is called once per frame
     void Update()
     {
-        switch (TagNum)
+
+        
+    }
+
+    public void Select()
+    {
+        if (TagNum == 1)
         {
-            case 1:
-                break;
-
-            case 2:
-                break;
-
-            case 3:
-                break;
-        };
+            VegetableShelf.SetActive(true);
+            FishMeatShelf.SetActive(false);
+            OthersShelf.SetActive(false);
+        }
+        if (TagNum == 2)
+        {
+            VegetableShelf.SetActive(false);
+            FishMeatShelf.SetActive(true);
+            OthersShelf.SetActive(false);
+        }
+        if (TagNum == 3)
+        {
+            VegetableShelf.SetActive(false);
+            FishMeatShelf.SetActive(false);
+            OthersShelf.SetActive(true);
+        }
     }
 }
