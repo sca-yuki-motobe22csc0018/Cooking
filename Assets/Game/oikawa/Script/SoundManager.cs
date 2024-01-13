8aaeaa16b1a6d8710bca6f9b79eb967fe5e03022
sosei <sdt22cge0016@edu.sca.ac.jp>
2024-01-13 18:11:49 +0900

    oikawa:サウンド重複問題の解消

--- Assets/Game/oikawa/Script/SoundManager.cs
+++ Assets/Game/oikawa/Script/SoundManager.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    void Start()
-    {
-        //  画面遷移後も音を鳴らし続ける(オブジェクトを壊れないようにする)
-        DontDestroyOnLoad(this);
+    //  シングルトン開始
+    public static SoundManager instance;
 
+    private void Awake()
+    {
+        CheckInstance();
     }
+
+    void CheckInstance()
+    {
+        if (instance == null)
+        {
+            instance = this;
+
+            //  画面遷移後も音を鳴らし続ける(オブジェクトを壊れないようにする)
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    //  シングルトン終わり
 }
