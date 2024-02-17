1bfdb720eb1359561a1bb126747b83b17dcf6fda
Yuki <sdt22csc0018@edu.sca.ac.jp>
2024-02-17 13:05:47 +0900

    a

--- Assets/Game/Motobe/Scripts/DifSkin.cs
+++ Assets/Game/Motobe/Scripts/DifSkin.cs
@@ -12,7 +12,7 @@ public class DifSkin : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        skinNum=1;
+        //skinNum=1;
     }
 
     // Update is called once per frame
